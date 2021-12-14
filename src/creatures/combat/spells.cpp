29e9a731002110e2fd1d2e2af85b4fbc2e02e098
Beats-Dh <daniel15042014@hotmail.com>
2021-12-13 23:46:19 -0400

    Fix spells behavior
    -Allow wave/beam spells to be called when standing in front of wall
    -Fix generated area of effect for wave/beam spells

--- src/creatures/combat/spells.cpp
+++ src/creatures/combat/spells.cpp
@@ -344,6 +344,7 @@ bool CombatSpell::castSpell(Creature* creature)
 
 		if (needDirection) {
 			var.pos = Spells::getCasterPosition(creature, creature->getDirection());
+			var.directionalArea = true;
 		} else {
 			var.pos = creature->getPosition();
 		}
@@ -354,11 +355,14 @@ bool CombatSpell::castSpell(Creature* creature)
 	Position pos;
 	if (needDirection) {
 		pos = Spells::getCasterPosition(creature, creature->getDirection());
+		combat->setDirectionArea(true);
+		combat->doCombat(creature, pos);
+		combat->setDirectionArea(false);
 	} else {
 		pos = creature->getPosition();
+		combat->doCombat(creature, pos);
 	}
 
-	combat->doCombat(creature, pos);
 	return true;
 }
 
@@ -374,6 +378,7 @@ bool CombatSpell::castSpell(Creature* creature, Creature* target)
 				var.pos = target->getPosition();
 			} else if (needDirection) {
 				var.pos = Spells::getCasterPosition(creature, creature->getDirection());
+				var.directionalArea = true;
 			} else {
 				var.pos = creature->getPosition();
 			}
@@ -730,9 +735,8 @@ bool Spell::playerInstantSpellCheck(Player* player, const Position& toPos)
 		g_game.map.setTile(toPos, tile);
 	}
 
-	ReturnValue ret = Combat::canDoCombat(player, tile, aggressive);
-	if (ret != RETURNVALUE_NOERROR) {
-		player->sendCancelMessage(ret);
+	if (aggressive && tile->hasFlag(TILESTATE_PROTECTIONZONE) && !player->hasFlag(PlayerFlag_IgnoreProtectionZone)) {
+		player->sendCancelMessage(RETURNVALUE_ACTIONNOTPERMITTEDINPROTECTIONZONE);
 		g_game.addMagicEffect(player->getPosition(), CONST_ME_POFF);
 		return false;
 	}
@@ -991,6 +995,7 @@ bool InstantSpell::playerCastInstant(Player* player, std::string& param)
 		} else {
 			var.type = VARIANT_POSITION;
 			var.pos = Spells::getCasterPosition(player, player->getDirection());
+			var.directionalArea = true;
 
 			if (!playerInstantSpellCheck(player, var.pos)) {
 				return false;
@@ -1034,6 +1039,7 @@ bool InstantSpell::playerCastInstant(Player* player, std::string& param)
 
 		if (needDirection) {
 			var.pos = Spells::getCasterPosition(player, player->getDirection());
+			var.directionalArea = true;
 		} else {
 			var.pos = player->getPosition();
 		}
@@ -1093,6 +1099,7 @@ bool InstantSpell::castSpell(Creature* creature)
 	} else if (needDirection) {
 		var.type = VARIANT_POSITION;
 		var.pos = Spells::getCasterPosition(creature, creature->getDirection());
+		var.directionalArea = true;
 	} else {
 		var.type = VARIANT_POSITION;
 		var.pos = creature->getPosition();
