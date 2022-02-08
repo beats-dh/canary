22ea48da536830ffe63485f4120354d8a44097bc
Beats-Dh <daniel15042014@hotmail.com>
2022-02-08 01:04:09 -0400

    teste

--- src/creatures/creature.cpp
+++ src/creatures/creature.cpp
@@ -730,7 +730,8 @@ bool Creature::dropCorpse(Creature* lastHitCreature, Creature* mostDamageCreatur
 			corpse->startDecaying();
 			bool corpses = corpse->isRewardCorpse() && (corpse->getID() == ITEM_MALE_CORPSE || corpse->getID() == ITEM_FEMALE_CORPSE);
 			
-			if (g_configManager().getBoolean(AUTOLOOT) && mostDamageCreature->getPlayer() == nullptr && !corpses) {
+			Player* mostDamageCreature = nullptr;
+			if (g_configManager().getBoolean(AUTOLOOT) && mostDamageCreature->getPlayer() && !corpses) {
 				int32_t pos = tile->getStackposOfItem(mostDamageCreature->getPlayer(), corpse);
 				g_dispatcher.addTask(createTask(std::bind(&Game::playerQuickLoot, &g_game, mostDamageCreature->getID(), this->getPosition(), corpse->getClientID(), pos, nullptr, false, true)));
 			}
