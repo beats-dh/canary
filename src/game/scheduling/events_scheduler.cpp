48f892a93d3a32935a96d39a8a02eb8738437514
Beats <daniel15042014@hotmail.com>
2023-07-11 23:02:24 -0300

    gha

--- src/game/scheduling/events_scheduler.cpp
+++ src/game/scheduling/events_scheduler.cpp
@@ -24,6 +24,7 @@ bool EventsScheduler::loadScheduleEventFromXml() {
 	}
 
 	time_t t = time(nullptr);
+
 	const tm* timePtr = localtime(&t);
 	int daysMath = ((timePtr->tm_year + 1900) * 365) + ((timePtr->tm_mon + 1) * 30) + (timePtr->tm_mday);
 
